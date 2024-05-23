3b042e63f31f2ea2308e2877643481f81ee848cb
Facundo Tobias Rey <103077968+reyfacundo@users.noreply.github.com>
2024-05-22 21:30:17 -0300

    rps

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.body.style.backgroundColor = "black";
+document.body.style.color = "white";
+
 
 function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3) + 1;
@@ -7,86 +9,39 @@ function getComputerChoice() {
     if (randomInt === 3) return "scissors";
 }
 
-console.log(getComputerChoice());
-
-function getHumanChoice() {
-    let rawUserInput = prompt("Rock, paper or scissors?");
-    let input = rawUserInput.split(' ').join('').toLowerCase();
-    if (input === "rock" || input === "paper" || input === "scissors") {
-        return input;
-    } else {
-        console.log("Invalid option. Please select either Rock, Paper, or Scissors.");
-        return null;
-    }
-}
 
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        playRound(e.target.id, getComputerChoice())
+    })
+})
+let humanScore = 0;
+let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        // if (humanChoice === null) {
-        //     console.log( "Invalid option, please select either Rock, Paper, or Scissors to start the game.")
-        // } else if (humanChoice === "rock" && computerChoice === "rock") {
-        //     console.log("It's a tie!")
-        //     console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-        // } else if (humanChoice === "rock" && computerChoice === "paper") {
-        //     console.log("You lose! Paper beats Rock")
-        //     computerScore++
-        //     console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-        // } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        //     console.log("You Win! Rock beats Scissors");
-        //     humanScore++
-        //     console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-        // } else if (humanChoice === "paper" && computerChoice === "rock") {
-        //     console.log("You win! Paper beats Rock")
-        //     humanScore++
-        //     console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-        // } else if (humanChoice === "paper" && computerChoice === "paper") {
-        //     console.log("It's a tie!")
-        //     console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-        // } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        //     console.log("You lose! Scissors beats Paper")
-        //     computerScore++
-        //     console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-        // } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        //     console.log("You lose! Rock beats Scissors")
-        //     computerScore++
-        //     console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-        // } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        //     console.log("You win! Scissors beats Paper")
-        //     humanScore++
-        //     console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-        // } else if (humanChoice === "scissors" && computerChoice === "scissors") {
-        //     console.log("It's a tie!")
-        //     console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-        // }
-        if (humanChoice !== null) {
-            if (humanChoice === computerChoice) {
-                console.log("It's a tie!");
-                console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-
-            } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
-                (humanChoice === "paper" && computerChoice === "rock") ||
-                (humanChoice === "scissors" && computerChoice === "paper")) {
-                console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-                humanScore++
-                console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-            } else {
-                console.log(`You Lose! ${humanChoice} beats ${computerChoice}`);
-                computerScore++
-                console.log(`Scoreboard- Player:${humanScore} - Computer:${computerScore}`)
-            }
+function playRound(humanChoice, computerChoice) {
+    let display = document.getElementById('display');
+    if (humanScore < 5 && computerScore < 5) {
+        if (humanChoice === computerChoice) {
+            display.innerHTML = `It's a tie!<br>Scoreboard- Player:${humanScore} - Computer:${computerScore}`
+        } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "paper" && computerChoice === "rock") ||
+            (humanChoice === "scissors" && computerChoice === "paper")) {
+            humanScore++
+            display.innerHTML = `You Win!  ${humanChoice} beats ${computerChoice} <br>Scoreboard- Player:${humanScore} - Computer:${computerScore}`
+        } else {
+            computerScore++
+            display.innerHTML = `You Lose!  ${computerChoice} beats ${humanChoice} <br>Scoreboard- Player:${humanScore} - Computer:${computerScore}`
         }
     }
-
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-    playRound(getHumanChoice(), getComputerChoice());
-
+    if (humanScore === 5) {
+        display.innerHTML = `5 points reached! You are the winner!<br>Final Score- Player:${humanScore} - Computer: ${computerScore}`
+        computerScore = 0
+        humanScore = 0
+    } else if (computerScore === 5) {
+        display.innerHTML = `You lost! The computer reached 5 points first! <br>Final Score- Player:${humanScore} - Computer: ${computerScore}`
+        computerScore = 0
+        humanScore = 0
+    }
 }
-
-playGame();
