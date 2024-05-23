564a9d4274ba14771ce5d4c3b22c7a0663c3a9d5
Facundo Tobias Rey <103077968+reyfacundo@users.noreply.github.com>
2024-05-22 23:10:25 -0300

    styles added

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-document.body.style.backgroundColor = "black";
-document.body.style.color = "white";
-
-
 function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3) + 1;
     if (randomInt === 1) return "rock";
@@ -22,25 +18,31 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     let display = document.getElementById('display');
+    let scoreboard = document.getElementById('scoreboard');
     if (humanScore < 5 && computerScore < 5) {
         if (humanChoice === computerChoice) {
-            display.innerHTML = `It's a tie!<br>Scoreboard- Player:${humanScore} - Computer:${computerScore}`
+            display.innerHTML = `It's a tie!`
+            scoreboard.innerHTML = `<span class="score">Scoreboard</span><br> Player:${humanScore} - Computer:${computerScore}`
         } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
             (humanChoice === "paper" && computerChoice === "rock") ||
             (humanChoice === "scissors" && computerChoice === "paper")) {
             humanScore++
-            display.innerHTML = `You Win!  ${humanChoice} beats ${computerChoice} <br>Scoreboard- Player:${humanScore} - Computer:${computerScore}`
+            display.innerHTML = `You Win!<br>  <span class="${humanChoice}">${humanChoice}</span> beats <span class="${computerChoice}">${computerChoice}</span>`
+            scoreboard.innerHTML = `<span class="score">Scoreboard</span><br> Player:${humanScore} - Computer:${computerScore}`
         } else {
             computerScore++
-            display.innerHTML = `You Lose!  ${computerChoice} beats ${humanChoice} <br>Scoreboard- Player:${humanScore} - Computer:${computerScore}`
+            display.innerHTML = `You Lose!<br> <span class="${computerChoice}">${computerChoice}</span> beats  <span class="${humanChoice}">${humanChoice}</span>`
+            scoreboard.innerHTML = `<span class="score">Scoreboard</span><br> Player:${humanScore} - Computer:${computerScore}`
         }
     }
     if (humanScore === 5) {
-        display.innerHTML = `5 points reached! You are the winner!<br>Final Score- Player:${humanScore} - Computer: ${computerScore}`
+        display.innerHTML = `5 points reached! You are the winner!`
+        scoreboard.innerHTML = `<span class="score">Final Score</span><br> Player:${humanScore} - Computer:${computerScore}`
         computerScore = 0
         humanScore = 0
     } else if (computerScore === 5) {
-        display.innerHTML = `You lost! The computer reached 5 points first! <br>Final Score- Player:${humanScore} - Computer: ${computerScore}`
+        display.innerHTML = `You lost!<br> The computer reached 5 points first!`
+        scoreboard.innerHTML = `<span class="score">Final Score</span><br> Player:${humanScore} - Computer:${computerScore}`
         computerScore = 0
         humanScore = 0
     }
